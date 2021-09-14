fb14f3d8fa3ed9845093b384444649d1cc312012
Rupmanjari <rupasinha11@gmail.com>
2021-09-14 15:54:38 +0530

    Exit Button code with Unity Editor #
    Exit Button can be used to close application by using Unity Editor  code # and Unity Editor namespace.

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,10 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(0);
+
+    }
 }
